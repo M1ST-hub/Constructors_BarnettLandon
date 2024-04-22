07b4ec0f935f73a827aa1ec43188d52fa903c1ed
M1ST-hub <nmckrypt@gmail.com>
2024-04-22 08:43:11 -0500

    Initial commit

---
+++ Constructors_BarnettLandon/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_BarnettLandon
+{
+    internal class Game
+    {
+
+        public string title, genre;
+        //Sets default params 
+        public Game(string titleParam = "Unkown Title", string genreParam = "Casual")
+        {
+            title = titleParam;
+            genre = genreParam;
+        }
+    }
+}
---
+++ Constructors_BarnettLandon/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constructors_BarnettLandon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates new object known as myGame
+            Game myGame = new Game("Apex Legends", "FPS"); 
+            //prints my favorite game to console
+            Console.WriteLine($"My favorite game is {myGame.title} and it is an {myGame.genre}");
+            //prints default params
+            Game myOtherGame = new Game();
+            Console.WriteLine($"My other favorite game is {myOtherGame.title} and it is a {myOtherGame.genre} gamed");
+        }
+    }
+}
